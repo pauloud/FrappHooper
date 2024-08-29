b725df33ee5cf6f8aa4cede8cb4b731ad4030de9
pauloud <paulfontaine2010@hotmail.fr>
2024-08-29 12:56:38 +0200

    remove elements to win space on mobile

--- dmloader.js
+++ dmloader.js
@@ -49,10 +49,6 @@ var CUSTOM_PARAMETERS = {
         var prevInnerWidth = -1;
         var prevInnerHeight = -1;
         
-        buttonHeight = 42;
-        
-        
-        buttonHeight = 42;
         
         // Hack for iOS when exit from Fullscreen mode
         if (is_iOS) {
